a84feca1d5e6c56f0fa55c16d97bb39c7c3cea9c
impressivecoder1 <rohanrahman251@gmail.com>
2026-04-28 23:05:48 +0600

    class one done

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -2,7 +2,6 @@
 const Home = () => {
     return (
         <div>
-           <h1>h</h1>
             <button className="btn btn-primary">cls</button>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,8 @@ const router = createBrowserRouter([
       {
         index: true,
         Component: Home
-      }
+      },
+      
     ]
   }
 ])
