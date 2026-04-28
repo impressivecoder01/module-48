b6242ea9f301ff81e57aa1fe9925401a0808d029
impressivecoder1 <rohanrahman251@gmail.com>
2026-04-28 11:20:16 +0600

    first commit

---
+++ src/Page/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div>
+           <h1>h</h1>
+            <button className="btn btn-primary">cls</button>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './Components/Layouts/Root.jsx';
+import Home from './Page/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Root></Root>,
+    children: [
+      {
+        index: true,
+        Component: Home
+      }
+    ]
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+      <RouterProvider router={router} />
+  </StrictMode>,
+)
